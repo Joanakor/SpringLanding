3c786dfac4a21aa89f3cd42e53b9ace3eef292b2
Joanakor <mark67769@gmail.com>
2025-09-10 12:13:02 +0300

    Initial commit

---
+++ src/main/java/com/example/SimpleTwoPageWebsiteOnSpring/SimpleTwoPageWebsiteOnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleTwoPageWebsiteOnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTwoPageWebsiteOnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTwoPageWebsiteOnSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleTwoPageWebsiteOnSpring/SimpleTwoPageWebsiteOnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleTwoPageWebsiteOnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTwoPageWebsiteOnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
